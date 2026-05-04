497d1eab39d74c2877dd2ed95b4113e0473091fe
Humoyun-py <sening_freif56@gmail.com>
2026-05-04 09:25:03 +0500

    change

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -34,7 +34,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
-            return request.user and request.user.is_authenticated
+            return True
         return (
             request.user
             and request.user.is_authenticated
