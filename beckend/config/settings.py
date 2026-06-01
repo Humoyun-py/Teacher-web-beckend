b7f8c21cbb5574a20e6964ab499bcd5d2381bc5b
Humoyun-py <sening_freif56@gmail.com>
2026-06-01 16:45:59 +0500

    It support panel qo'shildi

--- beckend/config/settings.py
+++ beckend/config/settings.py
@@ -133,7 +133,7 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = DEBUG
 CORS_ALLOWED_ORIGINS = os.environ.get(
     'CORS_ALLOWED_ORIGINS',
-    'https://makatab-teacher-web.netlify.app,http://localhost:5173,http://localhost:3000,http://127.0.0.1:5173'
+    'https://maktab-teacher-web.netlify.app/'
 ).split(',')
 
 # Swagger settings
